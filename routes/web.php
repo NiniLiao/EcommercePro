2bbd2d3cce403d3ff8574d78c2d13f86ecbe4c3a
Nini Liao <u9513031@cmu.edu.tw>
2023-07-17 15:16:52 +0800

    [feat.]added category data from admin panel

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,7 @@ Route::middleware([
 });
 
 Route::get('/redirect', [HomeController::class, 'redirect']);
+
+Route::get('/view_category', [AdminController::class, 'view_category']);
+
+Route::post('/add_category', [AdminController::class, 'add_category']);
