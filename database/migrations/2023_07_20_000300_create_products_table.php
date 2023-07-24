310ddfd844c5be4f09f262f4e0e0545acc1e2cc8
Nini Liao <u9513031@cmu.edu.tw>
2023-07-24 19:44:48 +0800

    modified title

--- database/migrations/2023_07_20_000300_create_products_table.php
+++ database/migrations/2023_07_20_000300_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('table')->nullable();
+            $table->string('title')->nullable();
             $table->string('description')->nullable();
             $table->string('image')->nullable();
             $table->string('category')->nullable();
