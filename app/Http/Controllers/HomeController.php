5f5ce8ab40691c7ac2598a713b1711c00bc9884b
Nini Liao <u9513031@cmu.edu.tw>
2023-07-28 16:17:55 +0800

    [feat.]cash order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 
 
 class HomeController extends Controller
@@ -86,5 +87,38 @@ class HomeController extends Controller
         $cart->delete();
 
         return redirect()->back();
+    }
+
+    public function cash_order()
+    {
+        $user = Auth::user();
+        $user_id = $user->id;
+        $data = Cart::where('user_id', '=', $user_id)->get();
+
+        foreach ($data as $data) {
+            $order = new Order;
+            $order->name = $data->name;
+            $order->email = $data->email;
+            $order->phone = $data->phone;
+            $order->address = $data->address;
+            $order->user_id = $data->user_id;
+
+            $order->product_title = $data->product_title;
+            $order->price = $data->price;
+            $order->quantity = $data->quantity;
+            $order->image = $data->image;
+            $order->product_id = $data->product_id;
+
+            $order->payment_status = 'cash on delivery';
+            $order->delivery_status = 'processing';
+
+            $order->save();
+
+            $cart_id = $data->id;
+            $cart = Cart::find($cart_id);
+            $cart->delete();
+        }
+
+        return redirect()->back()->with('message', 'We have received your order, we will connect with you soon...');
     }
 }
--- resources/views/home/show_cart.blade.php
+++ resources/views/home/show_cart.blade.php
@@ -55,6 +55,14 @@
          <!-- header section strats -->
          @include('home.header')
          <!-- end header section -->
+
+        @if(session()->has('message'))
+        <div class="alert alert-success">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            {{session()->get('message')}}
+            
+        </div>    
+        @endif   
          
       <div class="center">
         <table>
@@ -84,7 +92,7 @@
         </div>
         <div>
             <h1 style="font-size: 25px; padding-bottom: 15px;">Proceed to Order</h1>
-            <a href="" class="btn btn-danger">Cash On Delivery</a>
+            <a href="{{url('cash_order')}}" class="btn btn-danger">Cash On Delivery</a>
             <a href="" class="btn btn-danger">Pay Using Card</a>
         </div>
             
